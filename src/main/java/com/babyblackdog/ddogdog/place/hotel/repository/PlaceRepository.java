059a7e36f8b483ae1a06e6781f2a3a8898663b8f
SY97P <onetuks@naver.com>
2023-09-05 00:12:31 +0900

    feat: Place 도메인 레포지토리 작업

---
+++ src/main/java/com/babyblackdog/ddogdog/place/hotel/repository/PlaceRepository.java
@@ -0,0 +1,15 @@
+package com.babyblackdog.ddogdog.place.hotel.repository;
+
+import com.babyblackdog.ddogdog.place.hotel.model.Place;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+  @Query("select p from Place p where p.address.value =:address")
+  Page<Place> findPlaceByAddressContains(@Param("address") String address, Pageable pageable);
+
+}
