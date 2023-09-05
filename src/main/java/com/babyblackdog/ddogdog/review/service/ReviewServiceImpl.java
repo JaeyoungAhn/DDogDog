5fac9bdfea2eac532664fa58f06c75b5b19f321a
Jaeyoung Ahn <quaki@naver.com>
2023-09-05 14:28:45 +0900

    feat: Review 서비스 구현 추가

--- src/main/java/com/babyblackdog/ddogdog/review/service/ReviewServiceImpl.java
+++ src/main/java/com/babyblackdog/ddogdog/review/service/ReviewServiceImpl.java
@@ -1,28 +1,45 @@
 package com.babyblackdog.ddogdog.review.service;
 
+import com.babyblackdog.ddogdog.global.exception.ReviewException;
+import com.babyblackdog.ddogdog.reservation.model.Reservation;
+import com.babyblackdog.ddogdog.reservation.repository.ReservationRepository;
 import com.babyblackdog.ddogdog.review.model.Review;
+import com.babyblackdog.ddogdog.review.model.vo.Content;
+import com.babyblackdog.ddogdog.review.model.vo.Rating;
 import com.babyblackdog.ddogdog.review.repository.ReviewRepository;
 import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.babyblackdog.ddogdog.global.error.ErrorCode.INVALID_REVIEW;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
   private final ReviewRepository reviewRepository;
+  private final ReservationRepository reservationRepository;
 
   public ReviewServiceImpl(
-      ReviewRepository reviewRepository) {
+          ReviewRepository reviewRepository, ReservationRepository reservationRepository) {
     this.reviewRepository = reviewRepository;
+    this.reservationRepository = reservationRepository;
   }
 
   @Override
-  public Page<Review> findReviewsByRoomId(long roomId) {
-    return null;
+  public Page<ReviewResult> findReviewsByRoomId(long roomId, Pageable pageable) {
+    Page<Review> reviews = reviewRepository.findByRoomId(roomId, pageable);
+    return reviews.map(ReviewResult::of);
   }
 
   @Override
   public ReviewResult addReview(Long reservationId, String content, Double rating) {
-    return null;
+    Optional<Reservation> reservation = reservationRepository.findById(reservationId)
+            .orElseThrow(() -> new ReviewException(INVALID_REVIEW));
+    Review review = new Review(new Content(content), new Rating(rating), reservation.get());
+    Review savedReview = reviewRepository.save(review);
+    return ReviewResult.of(savedReview);
   }
 }
