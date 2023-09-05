f3ba188421e13c2fce5c0e49f238dc59cd5213b8
Jaeyoung Ahn <quaki@naver.com>
2023-09-05 14:30:06 +0900

    feat: Review 관련 DTO 구현

--- src/main/java/com/babyblackdog/ddogdog/review/controller/dto/ReviewResponse.java
+++ src/main/java/com/babyblackdog/ddogdog/review/controller/dto/ReviewResponse.java
@@ -1,5 +1,26 @@
 package com.babyblackdog.ddogdog.review.controller.dto;
 
-public class ReviewResponse {
+import com.babyblackdog.ddogdog.reservation.model.Reservation;
+import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
+
+import java.time.LocalDateTime;
+
+public record ReviewResponse(
+        Long id,
+        String content,
+        Double rating,
+        LocalDateTime createdDate,
+        Long reservation
+) {
+
+    public static ReviewResponse of(ReviewResult result) {
+        return new ReviewResponse(
+                result.id(),
+                result.content(),
+                result.rating(),
+                result.createdDate(),
+                result.reservation()
+        );
+    }
 
 }
--- src/main/java/com/babyblackdog/ddogdog/review/service/dto/ReviewResult.java
+++ src/main/java/com/babyblackdog/ddogdog/review/service/dto/ReviewResult.java
@@ -1,5 +1,30 @@
 package com.babyblackdog.ddogdog.review.service.dto;
 
-public class ReviewResult {
+
+import com.babyblackdog.ddogdog.place.PlaceResult;
+import com.babyblackdog.ddogdog.reservation.model.Reservation;
+import com.babyblackdog.ddogdog.review.model.Review;
+import com.babyblackdog.ddogdog.review.model.vo.Content;
+import com.babyblackdog.ddogdog.review.model.vo.Rating;
+
+import java.time.LocalDateTime;
+
+public record ReviewResult(
+        Long id,
+        String content,
+        Double rating,
+        LocalDateTime createdDate,
+        Long reservation
+) {
+
+    public static ReviewResult of(Review entity) {
+        return new ReviewResult(
+                entity.getId(),
+                entity.getContent(),
+                entity.getRating(),
+                entity.getCreatedDate(),
+                entity.getReservation()
+        );
+    }
 
 }
