ff0691cb725051a752fb7ea26b7ac6bb45654e11
Jaeyoung Ahn <quaki@naver.com>
2023-09-07 17:59:44 +0900

    rename: 패키지명 변경

--- src/main/java/com/babyblackdog/ddogdog/review/domain/Review.java
+++ src/main/java/com/babyblackdog/ddogdog/review/domain/Review.java
@@ -1,17 +1,12 @@
-package com.babyblackdog.ddogdog.review.model;
+package com.babyblackdog.ddogdog.review.domain;
 
-import com.babyblackdog.ddogdog.review.model.vo.Content;
-import com.babyblackdog.ddogdog.review.model.vo.Rating;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDate;
+import com.babyblackdog.ddogdog.review.domain.vo.Content;
+import com.babyblackdog.ddogdog.review.domain.vo.Rating;
+import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
