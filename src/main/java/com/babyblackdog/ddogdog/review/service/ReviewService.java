652a8887c7605038529ba90f92ad94f527af9c7e
Jaeyoung Ahn <quaki@naver.com>
2023-09-12 01:39:32 +0900

    feat: Review 서비스 기능 추가

--- src/main/java/com/babyblackdog/ddogdog/review/service/ReviewService.java
+++ src/main/java/com/babyblackdog/ddogdog/review/service/ReviewService.java
@@ -1,6 +1,9 @@
 package com.babyblackdog.ddogdog.review.service;
 
 import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
+import com.babyblackdog.ddogdog.reviewRoomReservationMapping.domain.ReviewRoomReservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
 
@@ -11,4 +14,28 @@ public interface ReviewService {
    * @return ReviewResult
    */
   ReviewResult registerReview(Long roomId, Long reservationId, String content, Double rating);
+
+  /**
+   * reviewId, content, rating 을 받아 리뷰 수정
+   *
+   * @param reviewId, content, rating
+   * @return ReviewResult
+   */
+  ReviewResult updateReview(Long reviewId, String content, Double rating);
+
+  /**
+   * userId, pageable을 통해 리뷰들 반환
+   *
+   * @param userId, pageable
+   * @return Page<ReviewResult>
+   */
+  Page<ReviewResult> findReviewsByUserId(Long userId, Pageable pageable);
+
+  /**
+   * Page<ReviewIds> 를 통해 리뷰들 반환
+   *
+   * @param reviewRoomReservations
+   * @return Page<ReviewResult>
+   */
+  Page<ReviewResult> findReviewsByReviewRoomReservation(Page<ReviewRoomReservation> reviewRoomReservations);
 }
--- src/main/java/com/babyblackdog/ddogdog/review/service/ReviewServiceImpl.java
+++ src/main/java/com/babyblackdog/ddogdog/review/service/ReviewServiceImpl.java
@@ -4,6 +4,9 @@ import com.babyblackdog.ddogdog.review.domain.Review;
 import com.babyblackdog.ddogdog.review.domain.vo.Content;
 import com.babyblackdog.ddogdog.review.domain.vo.Rating;
 import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
+import com.babyblackdog.ddogdog.reviewRoomReservationMapping.domain.ReviewRoomReservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,18 +15,40 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewServiceImpl implements ReviewService {
 
   private final ReviewStore store;
+  private final ReviewReader reader;
 
-  public ReviewServiceImpl(ReviewStore store) {
+  public ReviewServiceImpl(ReviewStore store, ReviewReader reader) {
     this.store = store;
+    this.reader = reader;
   }
 
   @Transactional
   @Override
-  public ReviewResult registerReview(Long roomId, Long reservationId, String content,
-      Double rating) {
+  public ReviewResult registerReview(Long roomId, Long reservationId, String content, Double rating) {
     Review review = new Review(new Content(content), new Rating(rating));
     Review savedReview = store.registerReview(review);
     return ReviewResult.of(savedReview);
+  }
+
+  @Transactional
+  @Override
+  public ReviewResult updateReview(Long reviewId, String content, Double rating) {
+    Review retrievedReview = reader.findReviewById(reviewId);
+    retrievedReview.setContent(new Content(content));
+    retrievedReview.setRating(new Rating(rating));
+    return ReviewResult.of(retrievedReview);
+  }
+
+  @Override
+  public Page<ReviewResult> findReviewsByUserId(Long userId, Pageable pageable) {
+    Page<Review> retrievedReviews = reader.findReviewsByUserId(userId, pageable);
+    return retrievedReviews.map(ReviewResult::of);
+  }
+
+  @Override
+  public Page<ReviewResult> findReviewsByReviewRoomReservation(Page<ReviewRoomReservation> reviewRoomReservations) {
+    Page<Review> retrievedReviews = reader.findReviewsByRoomId(reviewRoomReservations.get());
+    return retrievedReviews.map(ReviewResult::of);
   }
 
 }
