d88bbe7d39082d21049edefdc24d9d68f27c9034
Jaeyoung Ahn <quaki@naver.com>
2023-09-12 01:36:49 +0900

    feat: 3R 서비스 기능 추가

--- src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/service/ReviewRoomReservationService.java
+++ src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/service/ReviewRoomReservationService.java
@@ -1,5 +1,9 @@
 package com.babyblackdog.ddogdog.reviewRoomReservationMapping.service;
 
+import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ReviewRoomReservationService {
 
   /**
@@ -8,4 +12,12 @@ public interface ReviewRoomReservationService {
    * @param reservationId, reservationId, reviewId
    */
   void registerReviewRoomReservation(Long roomId, Long reservationId, Long reviewId);
+
+  /**
+   * roomId, pageable 을 받아 해당하는 Page<ReviewResult> 반환
+   *
+   * @param roomId, pageable
+   */
+  Page<ReviewResult> findReviewsByRoomId(Long roomId, Pageable pageable);
+
 }
--- src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/service/ReviewRoomReservationServiceImpl.java
+++ src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/service/ReviewRoomReservationServiceImpl.java
@@ -1,19 +1,29 @@
 package com.babyblackdog.ddogdog.reviewRoomReservationMapping.service;
 
+import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
 import com.babyblackdog.ddogdog.reviewRoomReservationMapping.domain.ReviewRoomReservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewRoomReservationServiceImpl implements ReviewRoomReservationService {
 
   private final ReviewRoomReservationStore store;
+  private final ReviewRoomReservationReader reader;
 
-  public ReviewRoomReservationServiceImpl(ReviewRoomReservationStore store) {
+  public ReviewRoomReservationServiceImpl(ReviewRoomReservationStore store, ReviewRoomReservationReader reader) {
     this.store = store;
+    this.reader = reader;
   }
 
   @Override
   public void registerReviewRoomReservation(Long roomId, Long reservationId, Long reviewId) {
     store.registerReviewRoomReservation(new ReviewRoomReservation(roomId, reservationId, reviewId));
+  }
+
+  @Override
+  public Page<ReviewResult> findReviewsByRoomId(Long roomId, Pageable pageable) {
+    Page<ReviewRoomReservation> reviewRoomReservations = reader.findReviewRoomReservationsByRoomId(roomId, pageable);
   }
 }
