b73c787f153a3bf235c953d67c563c18fe61fc6c
Jaeyoung Ahn <quaki@naver.com>
2023-09-12 01:40:26 +0900

    feat: Review 및 3R 인터페이스에 기능 추가

--- src/main/java/com/babyblackdog/ddogdog/review/repository/ReviewRepository.java
+++ src/main/java/com/babyblackdog/ddogdog/review/repository/ReviewRepository.java
@@ -1,10 +1,14 @@
 package com.babyblackdog.ddogdog.review.repository;
 
 import com.babyblackdog.ddogdog.review.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findReviewsByRoomId(Long roomId, Pageable pageable);
 
+    Page<Review> findReviewsByUserId(Long userId, Pageable pageable);
 }
--- src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/repository/ReviewRoomReservationRepository.java
+++ src/main/java/com/babyblackdog/ddogdog/reviewRoomReservationMapping/repository/ReviewRoomReservationRepository.java
@@ -1,6 +1,8 @@
 package com.babyblackdog.ddogdog.reviewRoomReservationMapping.repository;
 
 import com.babyblackdog.ddogdog.reviewRoomReservationMapping.domain.ReviewRoomReservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface ReviewRoomReservationRepository extends
     JpaRepository<ReviewRoomReservation, Long> {
 
+    Page<ReviewRoomReservation> findReviewRoomReservationsByRoomId(Long roomId, Pageable pageable);
 }
