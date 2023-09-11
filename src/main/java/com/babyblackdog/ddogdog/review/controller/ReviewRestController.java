1b26f1589fa0c051b14e6cfca28b8d0ebf38fea1
JaeyoungAhn <quaki@naver.com>
2023-09-12 05:54:17 +0900

    refactor: Review 컨트롤러 변수명 수정

--- src/main/java/com/babyblackdog/ddogdog/review/controller/ReviewRestController.java
+++ src/main/java/com/babyblackdog/ddogdog/review/controller/ReviewRestController.java
@@ -6,12 +6,17 @@ import com.babyblackdog.ddogdog.review.application.ReviewFacade;
 import com.babyblackdog.ddogdog.review.controller.dto.ReviewResponse;
 import com.babyblackdog.ddogdog.review.controller.dto.ReviewResponses;
 import com.babyblackdog.ddogdog.review.service.dto.ReviewResult;
+import com.babyblackdog.ddogdog.review.service.dto.ReviewResults;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReviewRestController {
@@ -24,30 +29,31 @@ public class ReviewRestController {
 
   @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ReviewResponse> createReview(@RequestParam Long roomId,
-                                                     @RequestParam Long reservationId,
-                                                     @RequestParam String content,
-                                                     @RequestParam Double rating) {
-    ReviewResult addedReview = facade.registerReview(roomId, reservationId, content, rating);
+      @RequestParam Long reservationId,
+      @RequestParam String content,
+      @RequestParam Double rating) {
+    ReviewResult addedReviewResult = facade.registerReview(roomId, reservationId, content, rating);
     return ResponseEntity
         .status(HttpStatus.CREATED)
-        .body(ReviewResponse.of(addedReview));
+        .body(ReviewResponse.of(addedReviewResult));
   }
 
   @PutMapping(value = "/{reviewId}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
   public ResponseEntity<ReviewResponse> modifyReview(@PathVariable Long reviewId,
-                                                     @RequestParam String content,
-                                                     @RequestParam Double rating) {
-    ReviewResult updatedReview = facade.updateReview(reviewId, content, rating);
+      @RequestParam String content,
+      @RequestParam Double rating) {
+    ReviewResult updatedReviewResult = facade.updateReview(reviewId, content, rating);
     return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(ReviewResponse.of(updatedReview));
+        .status(HttpStatus.CREATED)
+        .body(ReviewResponse.of(updatedReviewResult));
   }
 
   @GetMapping(value = "/{userId}", produces = APPLICATION_JSON_VALUE)
-  public ResponseEntity<ReviewResponses> getReviewsByUserId(@PathVariable Long userId) {
-    List<ReviewResult> retrievedReviews = facade.findReviewsByUserId(userId);
+  public ResponseEntity<ReviewResponses> getReviewsByUserId(@PathVariable Long userId,
+      Pageable pageable) {
+    ReviewResults retrievedReviewsResult = facade.findReviewsByUserId(userId, pageable);
     return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(ReviewResponse.of(retrievedReviews));
+        .status(HttpStatus.CREATED)
+        .body(ReviewResponses.of(retrievedReviewsResult));
   }
 }
