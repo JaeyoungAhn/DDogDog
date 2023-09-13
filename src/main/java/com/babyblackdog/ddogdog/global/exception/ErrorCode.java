ba2dafc407746de9bb2653429a9aa33a8ab25808
Jaeyoung Ahn <quaki@naver.com>
2023-09-13 16:47:47 +0900

    feat: Review 내 이메일 관련 에러 코드 추가

--- src/main/java/com/babyblackdog/ddogdog/global/exception/ErrorCode.java
+++ src/main/java/com/babyblackdog/ddogdog/global/exception/ErrorCode.java
@@ -1,7 +1,8 @@
 package com.babyblackdog.ddogdog.global.exception;
 
-import java.util.StringJoiner;
 import org.springframework.http.HttpStatus;
+
+import java.util.StringJoiner;
 
 public enum ErrorCode {
 
@@ -17,25 +18,23 @@ public enum ErrorCode {
   INVALID_PROVINCE_VALUE(HttpStatus.BAD_REQUEST, "PL-240", "지역 이름은 반드시 주어져야 합니다."),
 
   // Review
-  INVALID_RATING(HttpStatus.BAD_REQUEST, "R100", "유효하지 않은 별점입니다."),
-  INVALID_DECIMAL_POINT(HttpStatus.BAD_REQUEST, "R200", "유효하지 않은 소숫점 자리입니다."),
-  INVALID_REVIEW(HttpStatus.BAD_REQUEST, "R300", "유효하지 않은 리뷰입니다."),
-  EMPTY_REVIEW_CONTENT(HttpStatus.BAD_REQUEST, "R400", "유효하지 않은 리뷰 내용입니다."),
-  INVALID_REVIEW_LENGTH(HttpStatus.BAD_REQUEST, "R500", "리뷰 길이는 최소 10글자 이상이어야 합니다."),
-  INVALID_RATING_RANGE(HttpStatus.BAD_REQUEST, "R600", "유효하지 않은 리뷰 별점입니다."),
-  REVIEW_NOT_FOUND(HttpStatus.BAD_REQUEST, "7600", "존재하지 않는 리뷰입니다."),
+  REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "R-100", "존재하지 않는 리뷰입니다."),
+  INVALID_RATING(HttpStatus.BAD_REQUEST, "R-200", "유효하지 않은 별점입니다."),
+  INVALID_DECIMAL_POINT(HttpStatus.BAD_REQUEST, "R-210", "유효하지 않은 소숫점 자리입니다."),
+  INVALID_REVIEW(HttpStatus.BAD_REQUEST, "R-220", "유효하지 않은 리뷰입니다."),
+
+  INVALID_REVIEW_LENGTH(HttpStatus.BAD_REQUEST, "R-230", "리뷰 길이는 최소 10글자 이상이어야 합니다."),
+  INVALID_RATING_RANGE(HttpStatus.BAD_REQUEST, "R-240", "유효하지 않은 리뷰 별점입니다."),
+  INVALID_REVIEW_PARAMETER(HttpStatus.BAD_REQUEST, "R-250", "유효하지 않은 파라미터입니다."),
+  INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "R-260", "유효하지 않은 이메일입니다."),
+  EMPTY_REVIEW_CONTENT(HttpStatus.BAD_REQUEST, "R-300", "리뷰 내용이 비어있습니다."),
+  EMPTY_REVIEW_EMAIL(HttpStatus.BAD_REQUEST, "R-310", "이메일란이 비어있습니다."),
 
   // room
   ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "RM-100", "존재하지 않는 객실입니다."),
   INVALID_OCCUPANCY_LOWER(HttpStatus.BAD_REQUEST, "RM-200", "최대 수용 인원은 양수여야 합니다."),
   INVALID_OCCUPANCY_UPPER(HttpStatus.BAD_REQUEST, "RM-210", "최대 수용 인원은 10명까지 입니다."),
-  INVALID_ROOM_NUMBER(HttpStatus.BAD_REQUEST, "RM-220", "방 번호는 반드시 주어져야 합니다."),
-
-  // 3R(Review, Reservation, Room)
-  INVALID_ROOM_ID(HttpStatus.BAD_REQUEST, "RRR100", "유효하지 않은 객실번호입니다."),
-  INVALID_RESERVATION_ID(HttpStatus.BAD_REQUEST, "RRR200", "유효하지 않은 예약번호입니다."),
-  INVALID_REVIEW_ID(HttpStatus.BAD_REQUEST, "RRR300", "유효하지 않은 리뷰번호입니다."),
-  R3_NOT_FOUND(HttpStatus.BAD_REQUEST, "RRR400", "존재하지 않는 매핑테이블 키입니다.");
+  INVALID_ROOM_NUMBER(HttpStatus.BAD_REQUEST, "RM-220", "방 번호는 반드시 주어져야 합니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
