035f92a839f280f8f3ff1ff69ea4790faea29b1c
Jaeyoung Ahn <quaki@naver.com>
2023-09-19 12:47:54 +0900

    refactor: Wishlists 대신 단수 Wishlist 사용

--- src/main/java/com/babyblackdog/ddogdog/wishlist/controller/WishlistRestController.java
+++ src/main/java/com/babyblackdog/ddogdog/wishlist/controller/WishlistRestController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/wishlists")
+@RequestMapping(path = "/wishlist")
 public class WishlistRestController {
 
     private final WishlistFacade facade;
